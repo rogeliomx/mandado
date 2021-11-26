fa95a9035cc78ac3ab06955b98457c1d05e94b45
Rogelio Galindo <roge.galindo@gmail.com>
2021-11-26 13:52:12 -0600

    mandado alone

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import HolaMundoClass from './components/HolaMundoClass';
-import HolaMundo from './components/HolaMundo';
+
 
 import './App.css';
-import CicloDeVida from './components/CicloDeVida';
-import Characters from './components/Characters';
-import CouterHook from './components/HolaMundo/Counter'
-import CharactersHook from './components/Characters/CharactersHooks'
-import Reloj from './components/RelojWithHooks';
+
 import Todos from './components/Todo/Todos'
 
 
@@ -28,16 +23,9 @@ function App() {
         <header className="App-header">
           <Switch>
             <Route exact path="/">
-            <HolaMundo texto="Hola soy un stateless component"/>          
-             {/* <Todos texto="Hola soy un stateless component"/> */}
+             <Todos texto="Hola soy un stateless component"/>
             </Route>
-              <Route exact path="/class" component={HolaMundoClass}/>
-              <Route exact path="/notFound" render={() => <NotFound/>} />
-              <Route exact path="/lifeCycle" component={CicloDeVida} />
-              <Route exact path="/characters" component={Characters} />
-              <Route exact path="/counterHook" component={CouterHook} />
-              <Route exact path="/relojWithHook" component={Reloj} />
-              <Route exact path="/characterHook" component={CharactersHook} />
+        
             <Route exact path= "/todos" component={Todos} />
 
             <Redirect push to="/notFound" />
